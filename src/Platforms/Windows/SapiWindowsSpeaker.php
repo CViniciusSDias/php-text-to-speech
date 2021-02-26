8234494b199b4b6a52e36b91c7deef654581bfc5
Vinicius Dias <carlosv775@gmail.com>
2021-02-26 13:58:50 -0300

    Fixing windows speaker

--- src/Platforms/Windows/SapiWindowsSpeaker.php
+++ src/Platforms/Windows/SapiWindowsSpeaker.php
@@ -8,7 +8,7 @@ class SapiWindowsSpeaker implements Speaker
 {
     public function speak(string $text): void
     {
-        $voice = new com("SAPI.SpVoice");
-        $voice.Speak($text);
+        $voice = new \com("SAPI.SpVoice");
+        $voice->Speak($text);
     }
 }
